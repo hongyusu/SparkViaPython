d7d20bd78663170c146d27891b31b00aa1608453
hongyusu <hongyu.su@me.com>
2015-10-20 22:52:15 +0300

    minors and auto commit and upate

--- Examples/linear_regression.py
+++ Examples/linear_regression.py
@@ -4,6 +4,7 @@ from pyspark import SparkConf, SparkContext
 from pyspark.mllib.classification import SVMWithSGD, SVMModel
 from pyspark.mllib.classification import LogisticRegressionWithSGD, LogisticRegressionModel
 from pyspark.mllib.regression import  LinearRegressionWithSGD, LinearRegressionModel
+from pyspark.mllib.tree import RandomForest, RandomForestModel
 from pyspark.mllib.tree import DecisionTree,DecisionTreeModel
 from pyspark.mllib.regression import LabeledPoint
 from pyspark.mllib.util import MLUtils
@@ -17,8 +18,8 @@ def leastSquare(trainingData,testData,trainingSize,testSize):
   linear lr classifier
   '''
   # train a lr model
-  numIterValList = [1000,3000,5000]
-  stepSizeValList = [1e-11,1e-9,1e-7,1e-5]
+  numIterValList = [500,300,500]
+  stepSizeValList = [5e-6,1e-9,1e-11]
 
   # variable for the best parameters
   bestNumIterVal = 200
@@ -31,25 +32,26 @@ def leastSquare(trainingData,testData,trainingSize,testSize):
   for numIterVal,stepSizeVal in itertools.product(numIterValList,stepSizeValList):
     model = LinearRegressionWithSGD.train(trainingData, iterations=numIterVal, step=stepSizeVal, regParam=regParamVal, regType=regTypeVal)
     ValsAndPreds = trainingData.map(lambda p: (p.label, model.predict(p.features)))
-    trainingRMSE = math.sqrt(ValsAndPreds.map(lambda (v, p): (v - p)**2).reduce(lambda x, y: x + y) / trainingSize)
+    trainingRMSE = math.sqrt(ValsAndPreds.map(lambda (v, p): (v - p)**2).reduce(lambda x, y: x + y) / float(trainingSize))
     if trainingRMSE:
       if trainingRMSE<bestTrainingRMSE:
         bestNumIterVal = numIterVal
         bestStepSizeVal = stepSizeVal
         bestTrainingRMSE = trainingRMSE
     print numIterVal,stepSizeVal,trainingRMSE
+    return
   print bestNumIterVal,bestStepSizeVal,bestTrainingRMSE
 
   model = LinearRegressionWithSGD.train(trainingData, iterations=bestNumIterVal, step=bestStepSizeVal, regParam=regParamVal, regType=regTypeVal)
 
   # Evaluating the model on training data
   ValsAndPreds = trainingData.map(lambda p: (p.label, model.predict(p.features)))
-  trainingRMSE = math.sqrt(ValsAndPreds.map(lambda (v, p): (v - p)**2).reduce(lambda x, y: x + y) / trainingSize)
+  trainingRMSE = math.sqrt(ValsAndPreds.map(lambda (v, p): (v - p)**2).reduce(lambda x, y: x + y) / float(trainingSize))
   print trainingRMSE
 
   # Evaluating the model on training data
   ValsAndPreds = testData.map(lambda p: (p.label, model.predict(p.features)))
-  testRMSE = math.sqrt(ValsAndPreds.map(lambda (v, p): (v - p)**2).reduce(lambda x, y: x + y) / testSize)
+  testRMSE = math.sqrt(ValsAndPreds.map(lambda (v, p): (v - p)**2).reduce(lambda x, y: x + y) / float(testSize))
   print testRMSE
   pass
 
@@ -100,14 +102,14 @@ def decisionTreeRegression(trainingData,testData,trainingSize,testSize):
   '''
   # parameter range
   maxDepthValList = [10,20,30]
-  maxBinsVal = [16,24,32]
+  maxBinsValList = [16,24,32]
 
   # best parameters
   bestMaxDepthVal = 5
   bestMaxBinsVal = 16
   bestTrainingRMSE = 1e10
 
-  for maxDepthVal,maxBinsVal in itertools.product(maxDepthValList,maxBinsVal):
+  for maxDepthVal,maxBinsVal in itertools.product(maxDepthValList,maxBinsValList):
     model = DecisionTree.trainRegressor(trainingData,categoricalFeaturesInfo={},impurity='variance',maxDepth=maxDepthVal,maxBins=maxBinsVal)
     predictions = model.predict(trainingData.map(lambda x:x.features))
     ValsAndPreds = trainingData.map(lambda x:x.label).zip(predictions)
@@ -133,9 +135,51 @@ def decisionTreeRegression(trainingData,testData,trainingSize,testSize):
   ValsAndPreds = testData.map(lambda x:x.label).zip(predictions)
   testRMSE = math.sqrt(ValsAndPreds.map(lambda (v, p): (v - p)**2).reduce(lambda x, y: x + y) / testSize)
   print testRMSE
-
-
   pass
+
+def randomForestRegression(trainingData,testData,trainingSize,testSize):
+  '''
+  random forest for regression
+  '''
+  # parameter range
+  maxDepthValList = [10,20,30]
+  maxBinsValList = [16,24,32]
+  numTreesValList = [10,30,50]
+
+  # best parameters
+  bestMaxDepthVal = 10
+  bestMaxBinsVal = 16
+  bestNumTreesVal = 10
+  bestTrainingRMSE = 1e10
+
+  for maxDepthVal,maxBinsVal,numTreesVal in itertools.product(maxDepthValList,maxBinsValList,numTreesValList):
+    model = RandomForest.trainRegressor(trainingData,categoricalFeaturesInfo={},numTrees=numTreesVal,featureSubsetStrategy="auto",impurity='variance',maxDepth=maxDepthVal,maxBins=maxBinsVal)
+    predictions = model.predict(trainingData.map(lambda x:x.features))
+    ValsAndPreds = trainingData.map(lambda x:x.label).zip(predictions)
+    trainingRMSE = math.sqrt(ValsAndPreds.map(lambda (v, p): (v - p)**2).reduce(lambda x, y: x + y) / trainingSize)
+    if trainingRMSE:
+      if trainingRMSE<bestTrainingRMSE:
+        bestMaxDepthVal = maxDepthVal
+        bestMaxBinsVal = maxBinsVal
+        bestNumTreesVal = numTreesVal
+        bestTrainingRMSE = trainingRMSE
+    print maxDepthVal, maxBinsVal, numTreesVal, trainingRMSE
+  print bestMaxDepthVal,bestMaxBinsVal, bestNumTreesVal, bestTrainingRMSE
+
+  model = RandomForest.trainRegressor(trainingData,categoricalFeaturesInfo={},numTrees=bestNumTreesVal,featureSubsetStrategy="auto",impurity='variance',maxDepth=bestMaxDepthVal,maxBins=bestMaxBinsVal)
+
+  # evaluating the model on training data
+  predictions = model.predict(trainingData.map(lambda x:x.features))
+  ValsAndPreds = trainingData.map(lambda x:x.label).zip(predictions)
+  trainingRMSE = math.sqrt(ValsAndPreds.map(lambda (v, p): (v - p)**2).reduce(lambda x, y: x + y) / trainingSize)
+  print trainingRMSE
+
+  # evaluating the model on test data
+  predictions = model.predict(testData.map(lambda x:x.features))
+  ValsAndPreds = testData.map(lambda x:x.label).zip(predictions)
+  testRMSE = math.sqrt(ValsAndPreds.map(lambda (v, p): (v - p)**2).reduce(lambda x, y: x + y) / testSize)
+  print testRMSE
+
 
 if __name__ == '__main__':
 
@@ -150,7 +194,7 @@ if __name__ == '__main__':
   
   # load data from file
   parsedData = MLUtils.loadLibSVMFile(sc, "../Data/cadata")
-  parsedData = MLUtils.loadLibSVMFile(sc, "../Data/YearPredictionMSD")
+  #parsedData = MLUtils.loadLibSVMFile(sc, "../Data/YearPredictionMSD")
 
   # split data into training and test
   trainingData,testData = parsedData.randomSplit([0.8,0.2])
@@ -166,7 +210,8 @@ if __name__ == '__main__':
   #leastSquare(trainingData,testData,trainingSize,testSize)
   #regularized(trainingData,testData,trainingSize,testSize,'l1')
   #regularized(trainingData,testData,trainingSize,testSize,'l2')
-  decisionTreeRegression(trainingData,testData,trainingSize,testSize)
+  #decisionTreeRegression(trainingData,testData,trainingSize,testSize)
+  randomForestRegression(trainingData,testData,trainingSize,testSize)
 
   sc.stop()
 
